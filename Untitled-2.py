250aa5f339ca1f694058029a5e1328d82f1fb6ec
Yahya Ahmad <106219715+yhyaahmd@users.noreply.github.com>
2023-09-02 10:42:04 +0700

    Commit tugas rentetan 1

---
+++ Untitled-2.py
@@ -0,0 +1,59 @@
+#tempat menyimpan 
+
+#type data
+#float bilangan decimal ,
+apel = 52.1
+print(apel)
+print(type(apel))
+
+#integer bilangan bulat
+apel2 = 52
+print(apel2)
+print(type(apel2))
+
+#string kata/kalimat
+nama_apel = "jogja"
+print(nama_apel)
+print(type(nama_apel))
+
+#boolean benar dan salah 1 0
+apel_benar = True
+apel_salah = False
+print(apel_benar)
+print(type(apel_benar))
+print(apel_salah)
+print(type(apel_salah))
+
+#list simpan beberapa data bisa diubah
+list_warna_apel = ['hijau', 'merah', 'kuning']
+list_apel1 = ['manis', 2 , 3.0]
+print(list_warna_apel)
+print(type(list_warna_apel))
+print(list_apel1)
+print(type(list_apel1))
+print(list_apel1[2])
+
+#tuple tidak bisa diubah
+#ini_tuple = (2, 5.25, "satu")
+#print(tuple)
+#print(type(ini_tuple))
+
+#dictionary bisa menyimpan beberapa data dengan kunci dan nilai pakai {}
+#ini_dict = {'nama':'apel', 'asal':'malang'}
+#print(ini_dict)
+
+#nama_apel =
+#a1pel =
+
+#operator artimatika
+print(2*3)
+print(2+3)
+print(2/1)
+print(6/3)
+print(31/4)
+
+#pangkat(**)
+
+b = 39
+e = 4
+print(b // e)
